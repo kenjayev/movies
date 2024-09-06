4f3dd1236c437786cc4ee23011ab7f92a5721ff6
kenjayev <kenjayevazizbek2oo4@gmail.com>
2024-09-06 14:19:00 +0500

    first file structure

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import React from "react";
 import "./App.css";
 
 // Layouts Import
-import Header from "./layout/header/Header";
+import Header from "./layout/Header";
 import Main from "./layout/Main";
-import Footer from "./layout/footer/Footer";
+import Footer from "./layout/Footer";
 
 class App extends React.Component {
   render() {
